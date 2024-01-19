a118f7cf6d2e4e952f9d24c7a795b8e039fa04c0
cherylaitt <cherylai481@gmail.com>
2024-01-19 12:34:09 +0800

    fix bug

--- app/channels/chat_messages_channel.rb
+++ app/channels/chat_messages_channel.rb
@@ -1,5 +1,16 @@
 class ChatMessagesChannel < ApplicationCable::Channel
   def subscribed
     stream_from 'chat_messages_channel'
+    puts 'subscribed'
+  end
+
+  def unsubscribed
+    # Unsubscribe from the channel
+    stop_all_streams
+  end
+
+  def after_stop
+    # Unsubscribe from the channel when the server stops
+    unsubscribe_from_channel
   end
 end
--- app/javascript/channels/chat_messages_channel.js
+++ app/javascript/channels/chat_messages_channel.js
@@ -12,18 +12,19 @@ consumer.subscriptions.create(
       console.log("appendLine", data);
       const html = this.createLine(data);
       const element = document.getElementById("message-section");
-      // querySelector("[data-chat-room='Best Room']");
-      console.log("element", element);
       element.insertAdjacentHTML("beforeend", html);
     },
 
     createLine(data) {
       return `
         <article class="chat-line">
-          <span class="speaker">${data["sent_by"]}</span>
-          <span class="body">${data["body"]}</span>
+          <span class="body">${data["content"]}</span>
         </article>
       `;
+    },
+
+    rejected() {
+      console.log("rejected");
     },
   }
 );
